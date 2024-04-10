2debd3aa725e6dca1ce518daf8b4badb3ce0e65e
Peter Gabriel Bersamina <petergabrielbersamina4@gmail.com>
2024-04-10 16:29:56 +0800

    new commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// script.js
+
+// Get a reference to the button element
+var button = document.getElementById('myButton');
+
+// Add a click event listener to the button
+button.addEventListener('click', function() {
+    // Display an alert message when the button is clicked
+    alert('Button clicked!');
+});
